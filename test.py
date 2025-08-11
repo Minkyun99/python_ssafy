15981e6bcc89987aa516442f4ac8db54b4161339
김민경 <kyung865821@gmail.com>
2025-08-11 17:55:55 +0900

    250811

--- test.py
+++ test.py
@@ -1,52 +1,36 @@
+# 돌 뒤집기
+
 import sys
 sys.stdin = open('input.txt')
 
-# 함수 1 : 최대값 구하기 
-def find_max_value(random_array):
-    max_value = random_array[0]
-    for i in range(len(random_array)):
-        if max_value < random_array[i]:
-            max_value = random_array[i]
-    return max_value
-
-# 함수 2 : 최소값 구하기
-def find_min_value(random_array):
-    min_value = random_array[0]
-    for i in range(len(random_array)):
-        if min_value > random_array[i]:
-            min_value = random_array[i]
-    return min_value
-
-
-# 테스트 케이스 입력 받기
 T = int(input())
 
-# 테스트 케이스만큼 수행
 for time in range(1, T+1):
-    # 정수의 개수 = 배열의 길이 받기
-    N = int(input())
-    # 배열의 값 받기
+    N, M = map(int, input().split())
     arr = list(map(int, input().split()))
+    print(arr)
+    for _ in range(M):
+        i, j = map(int, input().split())
+        print(f'프린트  {i}, {j}')
+        for y in range(1, j+1):
+            a = (i-1)-y
+            b = (i-1)+y
 
-    # 정렬된 값을 받을 새로운 배열 할당
-    new_arr = []
+            if a < 0 or a >= N or b < 0 or b >= N:
+                continue
 
-    # 함수를 수행하되, 배열의 길이의 절반만 수행하기
-    # 최대값과 최소값을 함께 정렬하기 때문에 배열의 길이에서 절반만 수행해야 함
-    for i in range(5):
-        # 최대값, 최소값 함수 수행
-        max_value = find_max_value(arr)
-        min_value = find_min_value(arr)
+            print(a, arr[a])
+            print(b, arr[b])
+            if arr[a] == 0 and arr[b] == 0:
+                arr[a], arr[b] = 1, 1
+                print(arr)
+            elif arr[a] == 1 and arr[b] == 1:
+                arr[a], arr[b] = 0, 0
+                print(arr)
+            elif arr[a] != arr[b]:
+                continue
 
-        # 최대값 먼저 append한 이후에 최소값 append 함
-        new_arr.append(max_value)
-        new_arr.append(min_value)
 
-        # 정렬된 값은 기존 배열에서 삭제함
-        arr.remove(max_value)
-        arr.remove(min_value)
-        
     print(f'#{time}', end=' ')
-    for i in new_arr:
-        print(i, end=' ')
-    print()
+    for row in arr:
+        print(row, end=' ')
--- test1.py
+++ test1.py
@@ -1,44 +1,21 @@
-import sys
-sys.stdin = open('input2.txt')
-
-# 북 페이지 찾는 함수 작성
-def search_book_page(total_page, find_page):
-    # 이진탐색을 위한 start, end 변수 할당
-    # start를 1로 주어야 함 (주어진 조건)
-    start_page = 1
-    end_page = total_page
-
-    # 이진탐색 횟수를 세는 count 변수 할당
-    count = 0
-
-    # 이진탐색 시작
-    while start_page <= end_page:
-        # end_page가 홀수로 끝날 수도 있으니, 해당 값까지 반영해야 함
-        half = int((start_page + end_page) / 2)
-        if half == find_page:
-            return count
-            break
-        elif half > find_page:
-            end_page = half 
-            count += 1
-        elif half < find_page:
-            start_page = half 
-            count += 1
-    return count
-
 T = int(input())
 
+
 for time in range(1, T+1):
-    P, A, B = map(int,input().split())
 
-    result_A = search_book_page(P, A)
-    result_B = search_book_page(P, B)
+    N = int(input())
+    arr = list(map(int, input()))
 
-    # return count 변수가 작은 사람을 출력함
-    if result_A > result_B:
-        print(f'#{time} B')
-    elif result_A == result_B:
-        print(f'#{time} 0')
-    elif result_A < result_B:
-        print(f'#{time} A')
+    count = 0
+    max_value = 0
+    for i in range(N):
+        if arr[i] == 1:
+            count += 1
+            if max_value < count:
+                max_value = count
+        else:
+            if max_value < count:
+                max_value = count
+            count = 0
 
+    print(f'#{time} {max_value}')
