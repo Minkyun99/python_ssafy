931fdbae9be93e198103dc5359bec3d9be2cb4aa
minkyun99 <alsrud7412@naver.com>
2025-09-04 08:41:52 +0900

    250904

--- test.py
+++ test.py
@@ -4,53 +4,31 @@ sys.stdin = open('input.txt')
 T = int(input())
 
 for time in range(1, T + 1):
-    N, A, B = map(int, input().split())
+    N, M = map(int,input().split())
 
-    arr = list(map(int, input().split()))
-    arr = sorted(arr)
-    arr = arr[::-1]
+    arr = [list(input()) for _ in range(N)]
 
-    A_arr = [0] * N
-    B_arr = [0] * N
+    No_same = []
+    color = ['W', 'R', 'B']
+    count = [[0] * 3 for _ in range(N)]
+
     for i in range(N):
-        if N % 2 == 0:
-            if A > B:
-                for j in range(0, A, 2):
-                    A_arr[j] = 1
-            if A == B:
-                for j in range(0, N, 2):
-                    A_arr[j] = 1
+        result = [0] * 3
+        for j in range(M-1):
+            if arr[i][j] != arr[i][j+1]:
+                No_same.append(i)
+                break
+            if arr[i][j] == 'W':
+                result[0] += 1
+            elif arr[i][j] == 'R':
+                result[1] += 1
             else:
-                for j in range(0, B, 2):
-                    B_arr[j] = 1
-        else:
-            if A > B:
-                for j in range(0, N, 2):
-                    A_arr[j] = 1
-            else:
-                for j in range(0, N, 2):
-                    B_arr[j] = 1
-    result = 0
-    v_A =  1
-    v_B =  1
+                result[2] += 1
+        
+        for x in range(3):
+            count[i][x] = result[x]
+        
 
-    if A >= B:
-        for i in range(N):
-            if A_arr[i] == 1:
-                A_arr[i] = arr[i] * v_A
-                v_A += 1
-            else:
-                B_arr[i] = arr[i] * v_B
-                v_B += 1
-        result = sum(A_arr) + sum(B_arr)
-    else:
-        for i in range(N):
-            if B_arr[i] == 1:
-                B_arr[i] = arr[i] * v_B
-                v_B += 1
-            else:
-                A_arr[i] = arr[i] * v_A
-                v_A += 1
-        result = sum(A_arr) + sum(B_arr)
 
-    print(f'#{time} {result}')
+    print(No_same)
+    print(count)
--- test1.py
+++ test1.py
@@ -4,24 +4,102 @@ sys.stdin = open('input.txt')
 T = int(input())
 
 for time in range(1, T+1):
-    N = int(input())
-    arr = [0]
-    arr += list(map(int, input().split()))
+    N, M = map(int, input().split())
 
-    bulb = [0]* (N+1)
+    arr = [list(input()) for _ in range(N)]
+    pwd_result = []
+    pwd = ''
+    if M <= 26:
+        for i in range(N):
+            for j in range(M):
+                q = 0
+                while arr[i][j+q] != '0':
+                    pwd += arr[i][j+q]
+                    q += 1
+                    break
+                pwd_result.append(pwd)
 
-    count = 0
-    for i in range(1, N+1):
-        if arr[i] != bulb[i]:
-            count += 1
-            for j in range(i, N+1, i):
-                if bulb[j] == 1:
-                    bulb[j] = 0
-                else:
-                    bulb[j] = 1
-    print(f'#{time} {count}')
+    print(pwd_result)
 
-                
-                
+    hex_dict = {'0': '0000', '1': '0001', '2': '0010', '3': '0011',
+                '4': '0100', '5': '0101', '6': '0110', '7': '0111',
+                '8': '1000', '9': '1001', 'A': '1010', 'B': '1011',
+                'C': '1100', 'D': '1101', 'E': '1110', 'F': '1111'}
+    
+    result = ''
+    for i in range(len(pwd)):
+        result += hex_dict[pwd[i]]
+
+    result_2 = ''
+    r = 0
+    for j in range(len(result)-1, -1, -1):
+        if result[j] == '1':
+            result_2 = result[j-55:j+1]
+            break
+
+    ratio_result = []
+    str_str = ''
+
+    for x in range(0,len(result_2), 7):
+        for i in range(7):
+            str_str += result_2[x+i]
+        ratio_result.append([str_str])
+        str_str = ''
+
+    ratio = {0: [3,2,1,1], 1:[2,2,2,1], 2:[2,1,2,2], 
+             3:[1,4,1,1], 4:[1,1,3,2], 5:[1,2,3,1], 
+             6:[1,1,1,4], 7:[1,3,1,2], 8:[1,2,1,3], 
+             9:[3,1,1,2]}
+    
 
 
+    all_ratios = []
+    for item in ratio_result:
+
+        pattern = item[0]
+        
+        ratios = []
+        current_char = pattern[-1]
+        count = 1
+        
+        for i in range(len(pattern) - 2, -1, -1):
+            if pattern[i] == current_char:
+                count += 1
+            else:
+                ratios.append(count)
+                count = 1
+                current_char = pattern[i]
+        ratios.append(count)
+        all_ratios.append(ratios[::-1])
+
+    result_result = []
+    for i in range(len(all_ratios)):
+        for key, value in ratio.items():
+            if all_ratios[i] == value:
+                result_result.append(key)
+
+    p = 0
+    p_odd = 0
+    for i in range(len(result_result)):
+        if i % 2 == 0:
+            p += result_result[i]
+        else:
+            p_odd += result_result[i]
+
+    if (p*3)+p_odd % 10 == 0:
+        print(sum(result_result))
+
+
+
+
+            
+
+            
+
+            
+
+
+
+
+
+    
