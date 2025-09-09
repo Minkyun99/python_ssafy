1277d5d2cd7ec7c5ec89fe448fc9364e6a503b89
minkyun99 <alsrud7412@naver.com>
2025-09-09 17:50:35 +0900

    250909

--- test.py
+++ test.py
@@ -1,41 +1,91 @@
 import sys
 sys.stdin = open('input.txt')
 
+def break_wall(arr, N, W, H, x, y, result):
+    global max_value
+    if N == 0:
+        if max_value < result:
+            max_value = result
+        return 
+    
+    if x < 0 or x >= H or y < 0 or y >= W:
+        return
+
+
+    for i in range(H):
+        for j in range(W):
+            dr = [1, -1, 0, 0]
+            dc = [0, 0, 1, -1]
+
+            count = 1
+            q_arr = []
+            q_arr.append([i, j, arr[i][j]-1])
+
+            visited = [[0] * W for _ in range(H)]
+            visited[i][j] = 1
+
+            while q_arr:
+                a = q_arr.pop(0)
+
+                for z in range(a[2]):
+                    for idx in range(4):
+
+                        x = a[0] + dr[idx] * z
+                        y = a[1] + dc[idx] * z
+
+                        if x < 0 or x >= H or y < 0 or y >= W or arr[x][y] == 0:
+                            continue
+
+                        if visited[x][y] == 0:
+
+                            if arr[x][y] > 1:
+                                count += 1
+                                q_arr.append([x, y, arr[x][y]-1])
+                                visited[x][y] = visited[i][j] + 1
+                            elif arr[x][y] == 1:
+                                count += 1
+                                visited[x][y] = visited[i][j] + 1
+
+            break_wall(arr, N-1, W, H, i, j, result+count)
+
+
+
+
 
 T = int(input())
 
 for time in range(1, T+1):
-    N, M = map(int, input().split())
+    N, W, H = map(int, input().split())
 
-    container = list(map(int, input().split()))
-    truck = list(map(int, input().split()))
-
-    container = sorted(container)
-    truck = sorted(truck)
-
-    count = 0
-    
-    if len(container) > len(truck):
-        while truck:
-            a = container.pop()
-            b = truck.pop()
-            if a <= b:
-                count += a
-            else:
-                truck.append(b)
-    elif len(container) == len(truck):
-        for i in range(len(container)):
-            if container[i] <= truck[i]:
-                count += container[i]
-    else:
-        while container:
-            a = container.pop()
-            b = truck.pop()
-            if a <= b:
-                count += a
-            else:
-                truck.append(b)
+    arr = [list(map(int, input().split())) for _ in range(H)]
 
 
-    print(f'#{time} {count}')
-            
+
+    result = 0
+    max_value = 0
+    break_wall(arr, N, W, H, 0, 0, result)
+    print(max_value)
+
+
+
+
+
+
+
+
+
+                # if 1 >= arr[j][i] > 0:
+                #     count += arr[j][i]
+                # elif arr[j][i] > 1:
+                #     n = arr[j][i]
+                #     for idx in range(n):
+                #         x = i + idx*n
+                #         y = j + idx*n
+
+                #         if x < 0 or x >= N or y < 0 or y >= N or arr[x][y] > 1:
+                #             continue
+
+                #         count += arr[x][y]
+
+                #         if arr[x][y] > 1:
+                #             break_wall(arr, )
