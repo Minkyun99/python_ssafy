1187e26075840876b3311e876efee9a7c870abdc
minkyun99 <alsrud7412@naver.com>
2025-09-12 17:44:49 +0900

    250912

--- test.py
+++ test.py
@@ -1,37 +1,32 @@
 import sys
 sys.stdin = open('input.txt')
-import time as ti
 
-starts = ti.time()
-
-
-def stack_top(x, result):
-    global the_result, top
-
+def min_price(x, result):
+    global min_value
     if x == len(arr):
-        if result >= top:
-            the_result = min(result, the_result)
-        return the_result
-
-    stack_top(x+1, result)
-    stack_top(x+1, result + arr[x])
-
+        if result < min_value:
+            min_value = result
+        return
+     
+    if result > min_value:
+        return
+     
+ 
+    for i in range(len(arr)):
+        if not visited_x[i]:
+ 
+            visited_x[i] = 1       
+            min_price(i,result + arr[x][i])
+            visited_x[i] = 0
+         
+ 
 T = int(input())
-
 for time in range(1, T+1):
-    N, top = map(int, input().split())
-    arr = list(map(int, input().split()))
-
-    the_result = 200000
+    N = int(input())
+    arr = [list(map(int, input().split())) for _ in range(N)]
+    min_value = 15000
     result = 0
-
-    stack_top(0, result)
-
-    print(f'#{time} {the_result - top}')
-
-
-ends = ti.time()
-
-t = ends - starts
-
-print(t)
+    visited_x = [0] * len(arr)
+ 
+    min_price(0, result)
+    print(f'#{time} {min_value}')
--- test1.py
+++ test1.py
@@ -1,43 +1,38 @@
 import sys
 sys.stdin = open('input.txt')
 
-x1 = [0] * 3
-y1 = [0] * 3
-x2 = [0] * 3
-y2 = [0] * 3
 
-x1[0], y1[0], x2[0], y2[0] = map(int, input().split())
-x1[1], y1[1], x2[1], y2[1] = map(int, input().split())
-x1[2], y1[2], x2[2], y2[2] = map(int, input().split())
+T = int(input())
 
+for time in range(1, T+1):
+    N = int(input())
 
-rec_arr = [[0]*2000 for _ in range(2000)]
+    arr = [list(map(int, input().split())) for _ in range(N)]
 
-
-
-for idx in range(3):
-    for i in range(x1[idx]+1000, x2[idx]+1000):
-        for j in range(y1[idx]+1000, y2[idx]+1000):
-            rec_arr[i][j] += 1
-
-
-
-M = (x2[2] - x1[2]) * (y2[2] - y1[2])
-
-
-count = 0
-minus_count = 0
-for i in range(2000):
-    for j in range(2000):
-        if rec_arr[i][j] == 1:
-            count += 1
-        elif rec_arr[i][j] == 2:
-            minus_count += 1
-
-
-
-print(count - (M - minus_count))
-
-
+    def find_cart(x, result):
+        global min_value
+        
+        # 가지치기: 현재 결과가 이미 최솟값보다 크면 중단
+        if result >= min_value:
+            return
+        
+        if x == N:
+            if min_value > result:
+                min_value = result
+            return
+                    
+        for i in range(N):
+            if arr[x][i] == 0:  # 연결되지 않은 경우
+                continue
+            
+            if not visited[i]:  # 아직 방문하지 않은 경우
+                visited[i] = 1
+                find_cart(x+1, result + arr[x][i])
+                visited[i] = 0
+    result = 0
+    visited = [0] * N
+    min_value = 100000
+    find_cart(0, result)
+    print(min_value)
 
 
